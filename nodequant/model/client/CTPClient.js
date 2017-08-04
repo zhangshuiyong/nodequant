fd858e5977b5eee047f72a7804e4f2cbfb4309ca
zhangshuiyong <632386504@qq.com>
2017-08-04 14:54:47 +0800

    Password not right login error is undefined

--- nodequant/model/client/CTPClient.js
+++ nodequant/model/client/CTPClient.js
@@ -465,7 +465,7 @@ class ctpMdClient{
         //login的回调函数，先绑定登录的回调函数，再调用主动函数
         ctpMdClient.ctpMdApi.on("RspUserLogin",function (response,error,requestId,isLast) {
 
-            if(error==undefined ||error.ErrorID==0)
+            if(error.ErrorID==0)
             {
                 ctpMdClient.ctpClient.OnInfo("Market Front login successfully");
 
@@ -768,7 +768,7 @@ class ctpTdClient{
         //login的回调函数，登录的回调函数
         ctpTdClient.ctpTdApi.on("RspUserLogin",function (response,error,requestId,isLast) {
 
-            if(error==undefined || error.ErrorID==0)
+            if(error.ErrorID==0)
             {
                 ctpTdClient.ctpClient.OnInfo(ctpTdClient.ctpClient.ClientName+" Trade Front login successfully");
 
