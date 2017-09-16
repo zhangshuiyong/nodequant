bb691e74c148e836814af051c8eea8fce65ae878
zhangshuiyong <632386504@qq.com>
2017-09-16 12:23:40 +0800

    update strategy configuration

--- nodequant/userConfig.js
+++ nodequant/userConfig.js
@@ -11,43 +11,40 @@ System_DBConfig={
 //行情数据库配置
 
 MarketData_DBConfig={
-        /*
+/*
                Host:"127.0.0.1",
-               Port:6379,
-               Password:"123456"
-       */
+               Port:8888,
+               Password:""
+*/
 };
 
 //配置客户端,NodeQuant启动,会连接已经配置的交易客户端
 ClientConfig={
-       CTP:{
-           userID:"88888888",
-           password:"88888888",
-           brokerID:"9999",
-           mdAddress:"tcp://180.169.68.3:11213",
-           tdAddress:"tcp://180.169.68.3:11205"
-       }/*,
-       Sgit:{
-          userID:"88888888",
-          password: "88888888",
-          brokerID:"",
-          mdAddress: "tcp://116.228.215.210:10004",
-          tdAddress: "tcp://116.228.215.210:10003"
-       }*/
+    CTP:{
+        userID:"888888",
+        password:"888888",
+        brokerID:"9999",
+        mdAddress:"tcp://180.169.68.3:11213",
+        tdAddress:"tcp://180.169.68.3:11205"
+    }/*,
+    Sgit:{
+        userID:"888888",
+        password: "888888",
+        brokerID:"",
+        mdAddress: "tcp://140.206.81.6:27777",
+        tdAddress: "tcp://140.206.81.6:27776"
+    }*/
 };
 
 StrategyConfig={
     Strategys:[
         {
             name:"Demo",
-            className:"DemoStrategy",
+            className:"TestTickToFinishSendOrder_Strategy",
             symbols: {
-                "i1801":{
+                "Au(T+D)":{
                     //要配置在哪个交易客户端订阅该合约，因为上期CTP和飞鼠Sgit都可以交易商品期货
-                    clientName:SupportClients.CTP,
-                    fee:0.00006,
-                    closeTodayFee:0.00024,
-                    feeType:FeeType.TradeAmount
+                    clientName:SupportClients.Sgit
                 }
             },
             BarType:KBarType.Minute,  //K线类型是: 分钟
